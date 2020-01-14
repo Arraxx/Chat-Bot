228aad013d8c2256453f22bdd0f9f14c7fed1daf
Aryaman Puri <aryamanpuri@gmail.com>
2020-01-14 23:45:34 +0530

    WIP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,54 @@ import Bot from '../bot.png'
 export class Home extends Component {
   render() {
     return (
-      <div style={{ marginTop: "100px" }}>
-        <b>
-          Welcome
-          </b>
-        <div style={{ float: "right" }}>
-          <img src={Bot} alt="Chat Bot" />
+      <div class="how-section1" style={{ marginTop: "10px" }}>
+        <div class="row">
+          <div class="col-lg-6 how-img">
+            <img src="https://image.ibb.co/dDW27U/Work_Section2_freelance_img1.png" class="rounded-circle img-fluid" alt="" />
+          </div>
+          <div class="col-lg-6">
+            <h4>Find rewarding projects</h4>
+            <h4 class="subheading">GetLance is a great place to find more clients, and to run and grow your own freelance business.</h4>
+            <p class="text-muted">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations.
+                                Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work.
+                                            More and more success. The greater the success you have on projects, the more likely you are to get hired by clients that use GetLance.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <h4>Get hired quickly</h4>
+            <h4 class="subheading">GetLance makes it easy to connect with clients and begin doing great work.</h4>
+            <p class="text-muted">Streamlined hiring. GetLance’s sophisticated algorithms highlight projects you’re a great fit for.
+                Top Rated and Rising Talent programs. Enjoy higher visibility with the added status of prestigious programs.
+                                            Do substantial work with top clients. GetLance pricing encourages freelancers to use GetLance for repeat relationships with their clients.</p>
+          </div>
+          <div class="col-lg-6 how-img">
+            <img src="https://image.ibb.co/cHgKnU/Work_Section2_freelance_img2.png" class="rounded-circle img-fluid" alt="" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 how-img">
+            <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt="" />
+          </div>
+          <div class="col-lg-6">
+            <h4>Work efficiently, effectively.</h4>
+            <h4 class="subheading">With GetLance, you have the freedom and flexibility to control when, where, and how you work. Each project includes an online workspace shared by you and your client, allowing you to:</h4>
+            <p class="text-muted">Send and receive files. Deliver digital assets in a secure environment.
+                Share feedback in real time. Use GetLance Messages to communicate via text, chat, or video.
+                                            Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <h4>Get paid on time</h4>
+            <h4 class="subheading">All projects include GetLance Payment Protection — helping ensure that you get paid for all work successfully completed through the freelancing website.</h4>
+            <p class="text-muted">All invoices and payments happen through GetLance. Count on a simple and streamlined process.
+                Hourly and fixed-price projects. For hourly work, submit timesheets through GetLance. For fixed-price jobs, set milestones and funds are released via GetLance escrow features.
+                                            Multiple payment options. Choose a payment method that works best for you, from direct deposit or PayPal to wire transfer and more.</p>
+          </div>
+          <div class="col-lg-6 how-img">
+            <img src="https://image.ibb.co/gQ9iE9/Work_Section2_freelance_img4.png" class="rounded-circle img-fluid" alt="" />
+          </div>
         </div>
       </div>
     )
