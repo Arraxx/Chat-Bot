018a8768e733f3cdc0d788590d7888d70e93a352
anubhavsingh16 <anubhavssingh177@gmail.com>
2020-01-21 03:27:24 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,44 +8,27 @@ export class Home extends Component {
             <img src="https://image.ibb.co/dDW27U/Work_Section2_freelance_img1.png" class="rounded-circle img-fluid" alt="" />
           </div>
           <div class="col-lg-6">
-            <h4>Find rewarding projects</h4>
-            <h4 class="subheading">GetLance is a great place to find more clients, and to run and grow your own freelance business.</h4>
-            <p class="text-muted">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations.
-                                Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work.
-                                            More and more success. The greater the success you have on projects, the more likely you are to get hired by clients that use GetLance.</p>
+            <h2>Rape</h2>
+            <h3 class="subheading">Rape in India has been described by Radha Kumar as one of India's most common crimes against women</h3>
+            <p class="text-muted">According to National Crime Records Bureau data of 2012,[16] Gujarat has the lowest rape rate (0.8) while Mizoram had the highest rape rate with a value of 10.1. the National Average was at 2.1. The rates were calculated by National Crime Records Bureau as the number of incidents per 100,000 of the population</p>
           </div>
         </div>
-        <div class="row">
-          <div class="col-lg-6">
-            <h4>Get hired quickly</h4>
-            <h4 class="subheading">GetLance makes it easy to connect with clients and begin doing great work.</h4>
-            <p class="text-muted">Streamlined hiring. GetLance’s sophisticated algorithms highlight projects you’re a great fit for.
-                Top Rated and Rising Talent programs. Enjoy higher visibility with the added status of prestigious programs.
-                                            Do substantial work with top clients. GetLance pricing encourages freelancers to use GetLance for repeat relationships with their clients.</p>
-          </div>
-          <div class="col-lg-6 how-img">
-            <img src="https://image.ibb.co/cHgKnU/Work_Section2_freelance_img2.png" class="rounded-circle img-fluid" alt="" />
-          </div>
-        </div>
+        
         <div class="row">
           <div class="col-lg-6 how-img">
             <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt="" />
           </div>
           <div class="col-lg-6">
-            <h4>Work efficiently, effectively.</h4>
-            <h4 class="subheading">With GetLance, you have the freedom and flexibility to control when, where, and how you work. Each project includes an online workspace shared by you and your client, allowing you to:</h4>
-            <p class="text-muted">Send and receive files. Deliver digital assets in a secure environment.
-                Share feedback in real time. Use GetLance Messages to communicate via text, chat, or video.
-                                            Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
+            <h4>Dowry</h4>
+            <h4 class="subheading">Dowries are considered a major contributor towards violence against women in India. Some of these offences include physical violence, emotional abuses, and murder of brides and girls</h4>
+            <p class="text-muted">Most dowry deaths occur when the young woman, unable to bear the harassment and torture, commits suicide. Most of these suicides are by hanging, poisoning or by fire. Sometimes the woman is killed by setting her on fire - this is known as bride burning, and is sometimes disguised as suicide or accident.[20] In 2012, 8,233 dowry death cases were reported across India.[21] Dowry issues caused 1.4 deaths per year per 100,000 women in India</p>
           </div>
         </div>
         <div class="row">
           <div class="col-lg-6">
-            <h4>Get paid on time</h4>
-            <h4 class="subheading">All projects include GetLance Payment Protection — helping ensure that you get paid for all work successfully completed through the freelancing website.</h4>
-            <p class="text-muted">All invoices and payments happen through GetLance. Count on a simple and streamlined process.
-                Hourly and fixed-price projects. For hourly work, submit timesheets through GetLance. For fixed-price jobs, set milestones and funds are released via GetLance escrow features.
-                                            Multiple payment options. Choose a payment method that works best for you, from direct deposit or PayPal to wire transfer and more.</p>
+            <h4>Domestic violence</h4>
+            <h4 class="subheading">Domestic violence in India is endemic.Around 70% of women in India are victims of domestic violence, according to Renuka Chowdhury, former Union minister for Women and Child Development.</h4>
+            <p class="text-muted">The National Crime Records Bureau reveal that a crime against a woman is committed every three minutes, a woman is raped every 29 minutes, a dowry death occurs every 77 minutes, and one case of cruelty committed by either the husband or relative of the husband occurs every nine minutes.This occurs despite the fact that women in India are legally protected from domestic abuse under the Protection of Women from Domestic Violence Act</p>
           </div>
           <div class="col-lg-6 how-img">
             <img src="https://image.ibb.co/gQ9iE9/Work_Section2_freelance_img4.png" class="rounded-circle img-fluid" alt="" />
