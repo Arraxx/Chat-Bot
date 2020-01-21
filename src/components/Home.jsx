2006c3493658af5fe351b3cff2a478c34a8d283a
Aryaman Puri <aryamanpuri@gmail.com>
2020-01-21 15:37:46 +0530

    Revert "Update (#9)" (#10)
    This reverts commit 713f90984b25a7bd292f88cb58c06cc99cfee29b.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,80 +8,50 @@ export class Home extends Component {
             <img src="https://image.ibb.co/dDW27U/Work_Section2_freelance_img1.png" class="rounded-circle img-fluid" alt="" />
           </div>
           <div class="col-lg-6">
-            <h2>Rape</h2>
-            <h3 class="subheading">Rape in India has been described by Radha Kumar as one of India's most common crimes against women</h3>
-            <p class="text-muted">According to National Crime Records Bureau data of 2012,[16] Gujarat has the lowest rape rate (0.8) while Mizoram had the highest rape rate with a value of 10.1. the National Average was at 2.1. The rates were calculated by National Crime Records Bureau as the number of incidents per 100,000 of the population</p>
+            <h4>Find rewarding projects</h4>
+            <h4 class="subheading">GetLance is a great place to find more clients, and to run and grow your own freelance business.</h4>
+            <p class="text-muted">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations.
+                                Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work.
+                                            More and more success. The greater the success you have on projects, the more likely you are to get hired by clients that use GetLance.</p>
           </div>
         </div>
-        
+        <div class="row">
+          <div class="col-lg-6">
+            <h4>Get hired quickly</h4>
+            <h4 class="subheading">GetLance makes it easy to connect with clients and begin doing great work.</h4>
+            <p class="text-muted">Streamlined hiring. GetLance’s sophisticated algorithms highlight projects you’re a great fit for.
+                Top Rated and Rising Talent programs. Enjoy higher visibility with the added status of prestigious programs.
+                                            Do substantial work with top clients. GetLance pricing encourages freelancers to use GetLance for repeat relationships with their clients.</p>
+          </div>
+          <div class="col-lg-6 how-img">
+            <img src="https://image.ibb.co/cHgKnU/Work_Section2_freelance_img2.png" class="rounded-circle img-fluid" alt="" />
+          </div>
+        </div>
         <div class="row">
           <div class="col-lg-6 how-img">
             <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt="" />
           </div>
           <div class="col-lg-6">
-            <h2>Dowry</h2>
-            <h3 class="subheading">Dowries are considered a major contributor towards violence against women in India. Some of these offences include physical violence, emotional abuses, and murder of brides and girls</h3>
-            <p class="text-muted">Most dowry deaths occur when the young woman, unable to bear the harassment and torture, commits suicide. Most of these suicides are by hanging, poisoning or by fire. Sometimes the woman is killed by setting her on fire - this is known as bride burning, and is sometimes disguised as suicide or accident.[20] In 2012, 8,233 dowry death cases were reported across India.[21] Dowry issues caused 1.4 deaths per year per 100,000 women in India</p>
+            <h4>Work efficiently, effectively.</h4>
+            <h4 class="subheading">With GetLance, you have the freedom and flexibility to control when, where, and how you work. Each project includes an online workspace shared by you and your client, allowing you to:</h4>
+            <p class="text-muted">Send and receive files. Deliver digital assets in a secure environment.
+                Share feedback in real time. Use GetLance Messages to communicate via text, chat, or video.
+                                            Use our mobile app. Many features can be accessed on your mobile phone when on the go.</p>
           </div>
         </div>
         <div class="row">
           <div class="col-lg-6">
-            <h2>Domestic violence</h2>
-            <h3 class="subheading">Domestic violence in India is endemic.Around 70% of women in India are victims of domestic violence, according to Renuka Chowdhury, former Union minister for Women and Child Development.</h3>
-            <p class="text-muted">The National Crime Records Bureau reveal that a crime against a woman is committed every three minutes, a woman is raped every 29 minutes, a dowry death occurs every 77 minutes, and one case of cruelty committed by either the husband or relative of the husband occurs every nine minutes.This occurs despite the fact that women in India are legally protected from domestic abuse under the Protection of Women from Domestic Violence Act</p>
+            <h4>Get paid on time</h4>
+            <h4 class="subheading">All projects include GetLance Payment Protection — helping ensure that you get paid for all work successfully completed through the freelancing website.</h4>
+            <p class="text-muted">All invoices and payments happen through GetLance. Count on a simple and streamlined process.
+                Hourly and fixed-price projects. For hourly work, submit timesheets through GetLance. For fixed-price jobs, set milestones and funds are released via GetLance escrow features.
+                                            Multiple payment options. Choose a payment method that works best for you, from direct deposit or PayPal to wire transfer and more.</p>
           </div>
           <div class="col-lg-6 how-img">
             <img src="https://image.ibb.co/gQ9iE9/Work_Section2_freelance_img4.png" class="rounded-circle img-fluid" alt="" />
           </div>
         </div>
       </div>
-
-
-      <div class="row">
-          <div class="col-lg-6 how-img">
-            <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt="" />
-          </div>
-          <div class="col-lg-6">
-            <h2>Illegal drug trade</h2>
-            <h3 class="subheading">India is located between two major illicit opium producing centres in Asia – the Golden Crescent comprising Pakistan, Afghanistan and Iran and the Golden Triangle comprising Burma, Thailand and Laos. Because of such geographical location, India experiences large amount of drug trafficking through the borders. India is the world's largest producer of licit opium for the pharmaceutical trade. But an undetermined quantity of opium is diverted to illicit international drug markets.</h3>
-            <p class="text-muted">India is a transshipment point for heroin from Southwest Asian countries like Afghanistan and Pakistan and from Southeast Asian countries like Burma, Laos, and Thailand.[30] Heroin is smuggled from Pakistan and Burma, with some quantities transshipped through Nepal.[30] Most heroin shipped from India are destined for Europe.[30] There have been reports of heroin smuggled from Mumbai to Nigeria for further export.
-
-In Maharashtra, Mumbai is an important centre for distribution of drug.[31] The most commonly used drug in Mumbai is Indian heroin (called desi mal by the local population). Both public transportation (road and rail transportation) and private transportation are used for this drug trade.</p>
-          </div>
-        </div>
-
-
-
-  <div class="row">
-          <div class="col-lg-6 how-img">
-            <img src="https://image.ibb.co/ctSLu9/Work_Section2_freelance_img3.png" class="rounded-circle img-fluid" alt="" />
-          </div>
-          <div class="col-lg-6">
-            <h2>Arms trafficking</h2>
-            <h3 class="subheading">According to a joint report published by Oxfam, Amnesty International and the International Action Network on Small Arms (IANSA) in 2006, there are around 40 million illegal small arms in India out of approximately 75 million in worldwide circulation.[40] Majority of the illegal small arms make its way into the states of Bihar, Chhattisgarh, Uttar Pradesh, Jharkhand, Orissa and Madhya Pradesh.[40] In UP, a used AK-47 costs $3,800 in black market</h3>
-            <p class="text-muted">Chinese pistols are in demand in the illegal small arms market in India because they are easily available and cheaper.[40] This trend poses a significant problem for the states of Bihar, Uttar Pradesh, Jharkhand, Chhattisgarh, Orissa, Maharashtra, West Bengal, Karnataka and Andhra Pradesh which have influence of Naxalism.[40] The porous Indo-Nepal border is an entry point for Chinese pistols, AK-47 and M-16 rifles into India as these arms are used by the Naxalites who have ties to Maoists in Nepal.</p>
-          </div>
-        </div>
-
-
-
-
-
-        <div class="row">
-          <div class="col-lg-6 how-img">
-            <img src="https://image.ibb.co/dDW27U/Work_Section2_freelance_img1.png" class="rounded-circle img-fluid" alt="" />
-          </div>
-          <div class="col-lg-6">
-            <h2>Cyber crime</h2>
-            <h3 class="subheading">The Information Technology Act 2000 was passed by the Parliament of India in May 2000, aiming to curb cyber crimes and provide a legal framework for e-commerce transactions.[50] However Pavan Duggal, lawyer of Supreme Court of India and cyber law expert, viewed "The IT Act, 2000, is primarily meant to be a legislation to promote e-commerce.</h3>
-            <p class="text-muted"> It is not very effective in dealing with several emerging cyber crimes like cyber harassment, defamation, stalking and so on". Although cyber crime cells have been set up in major cities, Duggal noted the problem is that most cases remain unreported due to a lack of awareness.
-
-In 2001, India and United States had set up an India-US cyber security forum as part of a counter-terrorism dialogue</p>
-          </div>
-        </div>
-
-
-
     )
   }
 }
